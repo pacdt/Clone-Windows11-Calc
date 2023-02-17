e93bbfbae2032d9bb6998442168d83831876fb15
pacdt <mateus15do2@gmail.com>
2023-02-17 13:30:12 -0300

    Add Funcionalidades dos buttons e operadores

---
+++ index.js
@@ -0,0 +1,182 @@
+let display = document.getElementById("display");
+let miniDisplay = document.getElementById("miniDisplay");
+let displayIsZero = true;
+
+// let digitsCount = 0;
+
+// function updateDisplay(value) {
+// 	if (displayIsZero) {
+// 		display.innerHTML = value.toLocaleString("pt-BR");
+// 		miniDisplay.innerHTML;
+// 		displayIsZero = false;
+// 		digitsCount = 1;
+// 	} else {
+// 		const displayValue = display.innerHTML.toLocaleString("pt-BR");
+// 		if (displayValue.length < 11) {
+// 			display.innerHTML += value.toLocaleString("pt-BR");
+// 			digitsCount++;
+// 		}
+// 	}
+// }
+
+function updateDisplay(value) {
+	if (displayIsZero) {
+		display.innerHTML = removeDecimalPoint(value);
+		miniDisplay.innerHTML;
+		displayIsZero = false;
+	} else if (display.innerHTML.length < 11) {
+		display.innerHTML += removeDecimalPoint(value);
+	}
+	addThousandsSeparator();
+}
+function removeDecimalPoint(value) {
+	return value.toString().replace(".", "");
+}
+
+function addThousandsSeparator() {
+	const displayValue = display.innerHTML.replace(".", "");
+	const integerPart = displayValue.split(".")[0];
+	const decimalPart = displayValue.split(".")[1];
+	const integerPartWithSeparators = integerPart.replace(
+		/\B(?=(\d{3})+(?!\d))/g,
+		"."
+	);
+	if (decimalPart) {
+		display.innerHTML = `${integerPartWithSeparators}.${decimalPart}`;
+	} else {
+		display.innerHTML = integerPartWithSeparators;
+	}
+}
+
+function clearDisplay() {
+	display.innerHTML = "0";
+	displayIsZero = true;
+}
+function clearMiniDisplay() {
+	miniDisplay.innerHTML = "&nbsp;";
+}
+
+let firstNumber = null;
+let secondNumber = null;
+
+const button0 = document.getElementById("button0");
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+const button5 = document.getElementById("button5");
+const button6 = document.getElementById("button6");
+const button7 = document.getElementById("button7");
+const button8 = document.getElementById("button8");
+const button9 = document.getElementById("button9");
+const btVirgula = document.getElementById("virgula");
+const btClear = document.getElementById("limpar");
+const buttonAdd = document.getElementById("buttonAdd");
+const buttonSubtract = document.getElementById("buttonSubtract");
+const buttonClear = document.getElementById("buttonClear");
+const buttonEquals = document.getElementById("buttonEquals");
+
+buttonClear.addEventListener("click", function () {
+	display.innerHTML = "0";
+	displayIsZero = true;
+});
+
+btClear.addEventListener("click", function () {
+	clearDisplay();
+	clearMiniDisplay();
+});
+
+const buttons = [
+	"button0",
+	"button1",
+	"button2",
+	"button3",
+	"button4",
+	"button5",
+	"button6",
+	"button7",
+	"button8",
+	"button9",
+];
+
+for (let i = 0; i < buttons.length; i++) {
+	const button = document.getElementById(buttons[i]);
+	button.addEventListener("click", function () {
+		updateDisplay(buttons[i].replace("button", "").toLocaleString("pt-BR"));
+	});
+}
+
+const expressions = [buttonAdd, buttonSubtract, buttonDivide, buttonMultiply];
+
+buttonAdd.addEventListener("click", function () {
+	firstNumber = display.innerHTML;
+	clearDisplay();
+	miniDisplay.innerHTML = `${firstNumber} + `;
+	display.innerHTML = firstNumber;
+	secondNumber = display.innerHTML;
+	return Number(firstNumber.replace(/\./g, '')), console.log(Number(firstNumber.replace(/\./g, '')));
+});
+
+buttonMultiply.addEventListener("click", function () {
+	firstNumber = display.innerHTML;
+	clearDisplay();
+	miniDisplay.innerHTML = `${firstNumber} x `;
+	display.innerHTML = firstNumber;
+	secondNumber = display.innerHTML;
+	return firstNumber;
+});
+
+buttonDivide.addEventListener("click", function () {
+	firstNumber = display.innerHTML;
+	clearDisplay();
+	miniDisplay.innerHTML = `${firstNumber} &divide;`;
+	display.innerHTML = firstNumber;
+	secondNumber = display.innerHTML;
+	return firstNumber;
+});
+
+buttonSubtract.addEventListener("click", function () {
+	firstNumber = display.innerHTML;
+	clearDisplay();
+	miniDisplay.innerHTML = `${firstNumber} - `;
+	display.innerHTML = firstNumber;
+	secondNumber = display.innerHTML;
+	return firstNumber;
+});
+console.log(firstNumber, secondNumber);
+
+buttonEquals.addEventListener("click", function () {
+	if (miniDisplay.innerHTML == `${firstNumber} - `) {
+		secondNumber = display.innerHTML;
+		clearDisplay();
+		let result = firstNumber - secondNumber;
+		display.innerHTML = result.toLocaleString("pt-BR");
+		miniDisplay.innerHTML = `${firstNumber} - ${secondNumber} =`;
+		console.log(result);
+        
+        //Adição
+	} else if (miniDisplay.innerHTML == `${firstNumber} + `) {
+		secondNumber = display.innerHTML;
+        secondNumber.replace(/\./g, '')
+		clearDisplay();
+
+		let result = Number(firstNumber) + Number(secondNumber);
+		display.innerHTML = result.toLocaleString("pt-BR");
+		miniDisplay.innerHTML = `${firstNumber} + ${secondNumber} =`;
+
+	} else if (miniDisplay.innerHTML == `${firstNumber} x `) {
+		secondNumber = display.innerHTML;
+		clearDisplay();
+		let result = firstNumber * secondNumber;
+		display.innerHTML = result.toLocaleString("pt-BR");
+		miniDisplay.innerHTML = `${firstNumber} x ${secondNumber} =`;
+		console.log(result);
+	} else {
+		secondNumber = display.innerHTML;
+		clearDisplay();
+		let result = firstNumber / secondNumber;
+		display.innerHTML = result.toLocaleString("pt-BR");
+		miniDisplay.innerHTML = `${firstNumber} &divide; ${secondNumber} =`;
+		console.log(result);
+	}
+});
